a97a6d74a668f638081f25551e668cecfdc0736b
salvatore <salvatore.venticinque@unicampania.it>
2021-02-08 23:44:51 +0100

    doc sources updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,8 +14,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../gcsimulator'))
-sys.path.insert(0, os.path.abspath('../gcsimulator/bin'))
+sys.path.insert(0, os.path.abspath('../../dockers/gcsim/gcsimulator'))
+sys.path.insert(0, os.path.abspath('../../dockers/gcsim/gcsimulator/bin'))
 
 # -- Project information -----------------------------------------------------
 
@@ -84,7 +84,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
